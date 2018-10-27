9a38909119ede748d7f0bdd3fd49cfe5d44ae09b
Computer <proj4only@gmail.com>
2018-10-27 15:19:22 +0900

    commit data 2018/10/27  15:18  ver 1.0.1

--- app/src/main/java/com/test/stationalertapplication/GetLocationService.java
+++ app/src/main/java/com/test/stationalertapplication/GetLocationService.java
@@ -156,8 +156,8 @@ public class GetLocationService extends Service {
                 Log.d("2点間の距離", "\n" + String.valueOf(resultRadius) + "Km");
 
                 // AlertDialogが出まくるのを防ぐため、一度出たらcounterをインクリメントして出さなくする。
-                //if (resultRadius < alertLine) {
-                if(counter == 0){
+                //if (resultRadius < alertLine) {   //本番用
+                if(counter == 0){                   //テスト用
                     Intent intent = new Intent(getApplicationContext(), MyBroadcastReceiver.class);
                     // ブロードキャストレシーバーが反応する言葉みたいなやつ
                     // とりあえず適当な言葉を入れているだけなので変更可能
